b962fade082c67c72942083eb830e2aeb47460dd
oieioi <atsuinatsu.samuifuyu@gmail.com>
2019-04-10 16:27:52 +0900

    Add unit test

--- spec/masososo_spec.rb
+++ spec/masososo_spec.rb
@@ -5,7 +5,35 @@ RSpec.describe Masososo do
     expect(Masososo::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "String#similar_chars" do
+    subject { char.similar_chars(option) }
+    let(:option) { {} }
+
+    context 'ｿ' do
+      let(:char) { 'ｿ' }
+      it { is_expected.to eq %w[ﾘ ﾝ] }
+    end
+
+    context 'ｿ including self' do
+      let(:char) { 'ｿ' }
+      let(:option) { { including_self: true } }
+      it { is_expected.to eq %w[ｿ ﾘ ﾝ] }
+    end
+
+    context 'empty string' do
+      subject { -> { char.similar_chars } }
+      let(:char) { '' }
+      it { is_expected.to raise_error RuntimeError }
+    end
+  end
+
+  describe "String#masososo" do
+    subject { str.masososo(option) }
+    let(:option) { { including_self: false } }
+
+    context 'こんにちわ' do
+      let(:str) { 'こんにちわ' }
+      it { is_expected.to eq 'こんにさわ' }
+    end
   end
 end
